e8a230da5f17e87c6942d5127201adc55569c181
spela2112 <spela@profinda.com>
2017-09-10 13:11:08 +0100

    js file for index

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-<script type="text/javascript">
-jQuery(document).ready(function($) {
+$(document).ready(function(){
+	$('.first_profile').hide(function(){
+    });
+ });
 
-  $('.nav').onePageNav();
-  
+$(document).ready(function(){
+    $("#profile_button1").click(function(){
+        $(".first_profile").animate({
+            height: 'toggle'
+        });
+    });
 });
-</script>
+
+$(document).ready(function(){
+	$('.second_profile').hide(function(){
+    });
+ });
+
+$(document).ready(function(){
+    $("#profile_button2").click(function(){
+        $(".second_profile").animate({
+            height: 'toggle'
+        });
+    });
+});
+
+$(document).ready(function(){
+	$('.third_profile').hide(function(){
+    });
+ });
+
+$(document).ready(function(){
+    $("#profile_button3").click(function(){
+        $(".third_profile").animate({
+            height: 'toggle'
+        });
+    });
+});
